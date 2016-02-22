65b7c79ea3689158f9dbeb458118d11c23c56c20
dozf <wuliaodozf@163.com>
2016-02-22 21:15:45 +0800

    Update dateFormat.js

--- javascripts/dateFormat.js
+++ javascripts/dateFormat.js
@@ -1,56 +1,59 @@
-/** 
-* 对Date的扩展，将 Date 转化为指定格式的String * 月(M)、日(d)、12小时(h)、24小时(H)、分(m)、秒(s)、周(E)、季度(q)
-可以用 1-2 个占位符 * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
-* eg: 
-* (newDate()).pattern("yyyy-MM-dd hh:mm:ss.S")==> 2006-07-02 08:09:04.423 
-* (new Date()).pattern("yyyy-MM-dd E HH:mm:ss") ==> 2009-03-10 二 20:09:04 
-* (new Date()).pattern("yyyy-MM-dd EE hh:mm:ss") ==> 2009-03-10 周二 08:09:04 
-* (new Date()).pattern("yyyy-MM-dd EEE hh:mm:ss") ==> 2009-03-10 星期二 08:09:04 
-* (new Date()).pattern("yyyy-M-d h:m:s.S") ==> 2006-7-2 8:9:4.18 
-*/
-Date.prototype.pattern=function(fmt) { 
-  var o = { 
-     "M+" : this.getMonth()+1, //月份 
-     "d+" : this.getDate(), //日 
-     "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时 
-     "H+" : this.getHours(), //小时 
-     "m+" : this.getMinutes(), //分 
-     "s+" : this.getSeconds(), //秒 
-     "q+" : Math.floor((this.getMonth()+3)/3), //季度 
-     "S" : this.getMilliseconds() //毫秒 
-  }; 
-  var week = { 
-     "0" : "\u65e5", //日
-     "1" : "\u4e00", //一
-     "2" : "\u4e8c", 
-     "3" : "\u4e09", 
-     "4" : "\u56db", 
-     "5" : "\u4e94", 
-     "6" : "\u516d" //六
-  }; 
-  if(/(y+)/.test(fmt)){ 
-   fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length)); 
-  } 
-  if(/(E+)/.test(fmt)){ 
-  fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "\u661f\u671f" : "\u5468") : "")+week[this.getDay()+""]); 
-  } 
-  for(var k in o){ 
-    if(new RegExp("("+ k +")").test(fmt)){ 
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length))); 
+// 将日期类型转换成字符串型格式 yyyy-MM-dd hh:mm:ss
+ 
+function ChangeTimeToString ( DateIn )
+{
+    var Year = 0 ;
+    var Month = 0 ;
+    var Day = 0 ;
+    var Hour = 0 ;
+    var Minute = 0 ;
+    var Seconds = 0;
+    var CurrentDate = "" ;
+ 
+    // 初始化时间
+    Year = DateIn . getFullYear();
+    Month = DateIn . getMonth ()+ 1 ;
+    Day = DateIn . getDate ();
+    Hour = DateIn . getHours ();
+    Minute = DateIn . getMinutes ();
+    Seconds = DateIn .getSeconds();
+    CurrentDate = Year + "-" ;
+    if ( Month >= 10 ){
+        CurrentDate = CurrentDate + Month + "-" ;
+    }else {
+        CurrentDate = CurrentDate + "0" + Month + "-" ;
+    }
+    if ( Day >= 10 ){
+        CurrentDate = CurrentDate + Day ;
+    }else{
+        CurrentDate = CurrentDate + "0" + Day ;
+    }
+   
+    if ( Hour >= 10 ){
+        CurrentDate = CurrentDate + " " + Hour ;
+    }else{
+        CurrentDate = CurrentDate + " 0" + Hour ;
+    }
+    if ( Minute >= 10 ){
+        CurrentDate = CurrentDate + ":" + Minute ;
+    }else{
+        CurrentDate = CurrentDate + ":0" + Minute ;
+    }      
+    if ( Seconds >= 10 ){
+        CurrentDate = CurrentDate + ":" + Seconds ;
+    }else{
+        CurrentDate = CurrentDate + ":0" + Seconds ;
     } 
-  } 
-return fmt; 
-} 
+    return CurrentDate ;
+}
 
 //显示现在时刻
 function showNowDate(){
-  var nowDate = new Date().Format("yyyy-MM-dd EEE hh:mm:ss");
+  var nowDate = ChangeTimeToString(new Date());
   alert("nowDate:"+nowDate);
 	span=document.getElementById("nowDateId");//现在时刻
 	alert(nowDate);
 	span.innerHTML = nowDate;
 }
 showNowDate();
-function getStrings(){
-  return "nihao";
-}
+
