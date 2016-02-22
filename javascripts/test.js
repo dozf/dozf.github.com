9d9e3d62d253e4760aaaef138d049264c3196b8e
dozf <wuliaodozf@163.com>
2016-02-22 16:24:51 +0800

    Update test.js

--- javascripts/test.js
+++ javascripts/test.js
@@ -2,6 +2,6 @@ console.log('这是测试用的js');
 alert('jjjj');
 test();
 function test(){
-  span=document.getElementById("testId");/测试内容
+  span=document.getElementById("testId");//测试内容
   span.innerHTML = '测试ssss';
 }
